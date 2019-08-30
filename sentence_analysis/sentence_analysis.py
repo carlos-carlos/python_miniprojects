9cba864651d9592bb2f90fddff210b48ec82439b
carlos-carlos <carlh5462@fastmail.com>
2019-08-30 17:35:51 +0700

    Added dictionary object. Cleaned up output

--- sentence_analysis/sentence_analysis.py
+++ sentence_analysis/sentence_analysis.py
@@ -8,40 +8,36 @@ print('Hello and welcome to sentence analysis tool! Please enter a sentence belo
 sentence = input('Please input your sentence here: ')
 list(sentence)
 
-#store totals in a variable for testing
-count_total = 0
-count_u = 0
-count_l = 0
-punc_count = 0
-
-count_totals = {'total': , 'upper': ,'lower': ,'punct': , }
+#Store the count in a dictionary
+count_dict = { 'total':0 ,'lower': 0, 'upper': 0, 'punct':0,}
 
 # iterate through the list and each time search for a different target character
 # increment the count each time you find a target character
 for s in sentence:
-    count_total += 1
+    count_dict['total'] += 1
 
 for s in sentence:
     if s.islower():
-        count_l += 1
+        count_dict['lower'] += 1
 
 for s in sentence:
     if s.isupper():
-        count_u += 1
+        count_dict['upper'] += 1
 
 for s in sentence:
     if s in punctuation:
-        punc_count += 1
+        count_dict['punct'] += 1
 
 # Tell the user the tally for their query
 print('\n')
-print('Thank you for your input!\n')
-print('The total number of characters entered is: ' + str(count_total))
-print('The number of lowercase character is: ' + str(count_l))
-print('The number of uppercase characters is: ' + str(count_u))
-print('The number of punctuation character is: ' + str(punc_count))
+print('------Thank you for your input!------\n')
+print('The total number of characters entered is: ' + str(count_dict['total']))
+print('The number of lowercase characters is: ' + str(count_dict['lower']))
+print('The number of uppercase characters is: ' + str(count_dict['upper']))
+print('The number of punctuation marks is: ' + str(count_dict['punct']) + '\n')
+print('Thank you for using the sentence anaylsis tool! See you next time!')
 
-#TODO: Add the part that puts it in a dictionary
+
 
 
 
