fdf49082ba64047a1989ad355366e05d17024e52
carlos-carlos <carlh5462@fastmail.com>
2019-09-19 02:29:55 +0800

    changed the way winning is decided

--- rps_game/rps_game_final.py
+++ rps_game/rps_game_final.py
@@ -5,10 +5,10 @@ player_input = int(input('Please enter a number between 0 - 2: '))
 
 #Generate a random number 0-2 to use for the computer's hand
 
-computer = random.randint(0,3)
+computer = random.randint(0,2)
 
-# Make string representation of the player's hand
-def getPlayerHand(hand):
+#make string representaitons of the hands
+def makeHand(hand):
     if hand == 0:
         return 'scissor'
 
@@ -20,46 +20,36 @@ def getPlayerHand(hand):
 
     else:
         pass
-
-# Make string representation of the computer's hand
-def getCompHand(hand):
-    if hand == 0:
-        return 'scissor'
-    elif hand == 1:
-        return 'rock'
-    elif hand == 2:
-        return 'paper'
-    else:
-        pass
+#print(makeHand(player_input))
+#print(makeHand(computer))
 
 def winning(player, comp):
 	'''Checks to see who won'''
-	if player > comp:
-		return 'Congratulations Computer! You won!'
+	if player == 'scissor' and comp == 'paper' or player == 'rock' and comp == 'scissor' or player == 'paper' and comp == 'rock':
+		return 'Glory to the Humans! Player Wins!'
 
-	elif comp > player:
-		return 'Congratulations Player! You won!'
+	elif comp == 'scissor' and player == 'paper' or comp == 'rock' and player == 'scissor' or comp == 'paper' and player == 'rock':
+		return 'Glory to the Machines! Computer Wins!'
 
 	else:
 		return 'Hmmm, it appears we have  tie!'
 
-#check if the input is with the specified range
 if player_input not in range(0, 3):
     print('Thats not a number between 0 and 2')
 
 else:
 
     # call the function `get_hand` to get the string representation of the user's hand
-    a = getPlayerHand(player_input)
+    a = makeHand(player_input)
 
     # call the function `get_hand` to get the string representation of the computer's hand
-    b = getCompHand(computer)
+    b = makeHand(computer)
 
     # call the function `determine_winner` to figure out who won
-    winning(player_input,computer)
+    c = winning(makeHand(player_input),makeHand(computer))
 
     # print out the player hand and computer hand
-    print('Player threw ' + getPlayerHand(player_input) + ' and Computer threw ' + getCompHand(computer) + '\n')
+    print('Player threw ' + makeHand(player_input) + ' and Computer threw ' + makeHand(computer) + '\n')
 
     # print out the winner
-    print(winning(getPlayerHand(player_input), getCompHand(computer)))
+    print(winning(makeHand(player_input), makeHand(computer)))
